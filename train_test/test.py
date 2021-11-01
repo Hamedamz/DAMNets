86c76342809edb34d780e977aab53070d62a1935
Hamed AMZ <amzhamed@gmail.com>
2021-11-01 21:12:53 +0330

    fix: add plasticity = 1 - p

--- train_test/test.py
+++ train_test/test.py
@@ -38,7 +38,7 @@ def test(net, source_dataloader, target_dataloader, device):
         # Setup hyperparameters
         p = float(batch_idx) / len(target_dataloader)
         lamda = 2. / (1. + np.exp(-10 * p)) - 1.
-        plasticity = 1
+        plasticity = 1 - p
         # Get input data along with corresponding label
         target_input, target_label = source_data
         # Transfer data to PyTorch tensor and define PyTorch Variable for target predicted labels
